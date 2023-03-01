c652de5cd61c805d282465b095fb6e629d278d7f
Sebastian-echeverria <98341740+sei-secheverria@users.noreply.github.com>
2023-03-01 13:49:14 -0500

    Added unit tests for ExternalMeasurement

---
+++ test/measurement/test_external_measurement.py
@@ -0,0 +1,46 @@
+"""
+Unit test for ExternalMeasurement.
+"""
+import pytest
+
+from mlte.measurement.result import Integer
+from mlte.measurement import (
+    MeasurementMetadata,
+    Identifier,
+    ExternalMeasurement,
+)
+
+
+def _dummy_calculation(x: int, y: int):
+    """
+    Dummy calculation function that adds two ints and multiplies the result by two.
+
+    :param x: The first number
+    :type x: int
+    :param y: The second number
+    :type y: int
+
+    :return: the result of the calculation
+    :rtype: int
+    """
+    return (x + y) * 2
+
+
+def test_evaluate_external():
+    x = 1
+    y = 2
+    expected_value = _dummy_calculation(x, y)
+    expected_result = Integer(
+        MeasurementMetadata("dummy", Identifier("test")), expected_value
+    )
+
+    measurement = ExternalMeasurement("dummy", Integer)
+    result = measurement.evaluate(_dummy_calculation(x, y))
+
+    assert isinstance(result, Integer)
+    assert result == expected_result
+
+
+def test_invalid_result_type():
+    with pytest.raises(Exception):
+        ExternalMeasurement("dummy", int)
