be610914f3b2bcfa97cb81fc7f0fb04610a0211c
Sebastian-echeverria <98341740+sei-secheverria@users.noreply.github.com>
2023-03-01 13:14:30 -0500

    Formatting and type check cleanup.

--- src/mlte/measurement/__init__.py
+++ src/mlte/measurement/__init__.py
@@ -9,5 +9,5 @@ __all__ = [
     "MeasurementMetadata",
     "Identifier",
     "ProcessMeasurement",
-    "ExternalMeasurement"
+    "ExternalMeasurement",
 ]
--- src/mlte/measurement/external_measurement.py
+++ src/mlte/measurement/external_measurement.py
@@ -1,23 +1,31 @@
-from typing import Type
+"""
+Base class for measurements calculated by external functions.
+"""
+
+from __future__ import annotations
 
 from .result import Result
 from .measurement import Measurement
 
 
 class ExternalMeasurement(Measurement):
-
-    def __init__(self, identifier: str, result_type: Type):
+    def __init__(self, identifier: str, result_type: type):
         """
         Initialize a new ExternalMeasurement measurement.
 
         :param identifier: A unique identifier for the instance
         :type identifier: str
         :param result_type: The type of the Result this measurement will return.
-        :type result_type: Type        
+        :type result_type: Type
         """
         super().__init__(self, identifier)
-        self.result_type: Type = result_type
-    
+        if not issubclass(Result, result_type):
+            raise Exception(
+                f"Result type provided is not a subtype of Result: {self.result_type}"
+            )
+        self.result_type: type = result_type       
+
     def __call__(self, *args, **kwargs) -> Result:
         """Evaluate a measurement and return results without semantics."""
-        return self.result_type(self.metadata, *args, **kwargs)
+        result: Result = self.result_type(self.metadata, *args, **kwargs)
+        return result
