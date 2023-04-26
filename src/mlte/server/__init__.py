0e90ca39376d4f26d15e341d7faefece9579e590
Alex Derr <aderr@sei.cmu.edu>
2023-04-26 16:02:15 -0400

    Set up nuxt for static hosting

--- src/mlte/server/__init__.py
+++ src/mlte/server/__init__.py
@@ -3,35 +3,33 @@ import os
 from flask import Flask, send_from_directory
 
 
-# # TODO : Update name of the directory
-REL_STATIC_DIR = "react-app/build"
+REL_STATIC_DIR = "nuxt-app/.output/public"
 
 app = Flask(__name__, static_folder=REL_STATIC_DIR)
 STATIC_DIR = os.path.join(app.root_path, REL_STATIC_DIR)
-NUXT_STATIC_DIR = os.path.join(app.root_path, 'nuxt-app/')
 
-# @app.route(_add_static_prefix("/"))
-# def serve():
-#     print(os.path.join(STATIC_DIR, "index.html"), flush=True)
-#     # /Users/aderr/repos/open-source-repos/mlte/src/mlte/server/react-app/build/index.html
+@app.route('/')
+def serve():
+    print(os.path.join(STATIC_DIR, 'index.html'), flush=True)
+    # /Users/aderr/repos/open-source-repos/mlte/env/lib/python3.8/site-packages/mlte/server/nuxt-app/index.html
 
-#     if os.path.exists(os.path.join(STATIC_DIR, "index.html")):
-#         return send_from_directory(STATIC_DIR, "index.html")
-#     else:
-#         return "Path failed", 200
+    if os.path.exists(os.path.join(STATIC_DIR, "index.html")):
+        return send_from_directory(STATIC_DIR, "index.html")
+    else:
+        return "Path failed", 200
 
 
-# def _run_server(static_prefix=None):
-#     """
-#     :param static_prefix: If set, the index.html asset will be served from the static_prefix.
-#                           If left None, the index.html asset will be served from the root path.
-#     :return: None
-#     """
-#     env_map = {}
-#     if static_prefix:
-#         env_map[STATIC_PREFIX_ENV_VAR] = static_prefix
+def run_server():
+    """
+    :param static_prefix: If set, the index.html asset will be served from the static_prefix.
+                          If left None, the index.html asset will be served from the root path.
+    :return: None
+    """
+    # env_map = {}
+    # if static_prefix:
+        # env_map[STATIC_PREFIX_ENV_VAR] = static_prefix
 
-#     app.run("0.0.0.0", "80")
+    app.run("0.0.0.0", "3000")
 
 
 import os
@@ -40,7 +38,7 @@ import subprocess
 def start_nuxt():
     print(os.getcwd(), flush=True)
     print(os.path.abspath('nuxt-app'), flush=True)
-    print(os.path.join(NUXT_STATIC_DIR))
+    print(os.path.join(STATIC_DIR))
 
-    os.chdir(os.path.join(NUXT_STATIC_DIR))
+    os.chdir(os.path.join(STATIC_DIR))
     subprocess.call(['node', '.output/server/index.mjs'])
