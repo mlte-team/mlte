01232e80bac3336b1c8374545be3497ff98b9ac9
Alex Derr <aderr@sei.cmu.edu>
2023-04-28 14:01:25 -0400

    Fix static pathing to nuxt files

--- src/mlte/server/__init__.py
+++ src/mlte/server/__init__.py
@@ -10,35 +10,18 @@ STATIC_DIR = os.path.join(app.root_path, REL_STATIC_DIR)
 
 @app.route('/')
 def serve():
-    print(os.path.join(STATIC_DIR, 'index.html'), flush=True)
-    # /Users/aderr/repos/open-source-repos/mlte/env/lib/python3.8/site-packages/mlte/server/nuxt-app/index.html
-
+    print(os.path.join(STATIC_DIR, "index.html"), flush=True)
+    # /Users/aderr/repos/open-source-repos/mlte/env/lib/python3.8/site-packages/mlte/server/nuxt-app/.output/public/index.html
     if os.path.exists(os.path.join(STATIC_DIR, "index.html")):
         return send_from_directory(STATIC_DIR, "index.html")
     else:
-        return "Path failed", 200
+        return "Path failed", 500
+    
+
+@app.route("/<path:path>")
+def serve_static_file(path):
+    return send_from_directory(STATIC_DIR, path)
 
 
 def run_server():
-    """
-    :param static_prefix: If set, the index.html asset will be served from the static_prefix.
-                          If left None, the index.html asset will be served from the root path.
-    :return: None
-    """
-    # env_map = {}
-    # if static_prefix:
-        # env_map[STATIC_PREFIX_ENV_VAR] = static_prefix
-
     app.run("0.0.0.0", "3000")
-
-
-import os
-import subprocess
-
-def start_nuxt():
-    print(os.getcwd(), flush=True)
-    print(os.path.abspath('nuxt-app'), flush=True)
-    print(os.path.join(STATIC_DIR))
-
-    os.chdir(os.path.join(STATIC_DIR))
-    subprocess.call(['node', '.output/server/index.mjs'])
