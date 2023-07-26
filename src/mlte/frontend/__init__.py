01b0203f4163df36605c05618cec1420dbd33c1c
Alex Derr <aderr@sei.cmu.edu>
2023-07-26 14:02:27 -0400

    Fix for failing CLI test

--- src/mlte/frontend/__init__.py
+++ src/mlte/frontend/__init__.py
@@ -5,13 +5,18 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
 
-app = FastAPI()
-STATIC_DIR = (
-    os.path.dirname(os.path.abspath(__file__)) + "/nuxt-app/.output/public"
-)
-
-app.mount("/", StaticFiles(directory=STATIC_DIR, html=True), name="static")
-
-
 def run_frontend():
-    uvicorn.run(app)
+    app = FastAPI()
+    static_dir = (
+        os.path.dirname(os.path.abspath(__file__)) + "/nuxt-app/.output/public"
+    )
+
+    if os.path.exists(static_dir):
+        app.mount(
+            "/", StaticFiles(directory=static_dir, html=True), name="static"
+        )
+        uvicorn.run(app)
+    else:
+        print(
+            "Unable to run frontend. This is likely due to an issue with this distribution of the package."
+        )
