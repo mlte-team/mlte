d6e36e5e0d06754cc6b7ad0eb89ab11cccc980c6
Sebastian-echeverria <98341740+sei-secheverria@users.noreply.github.com>
2023-08-18 13:58:20 -0400

    Base model to Spec and constituent components. Ignoring/bypassing Requirement since it doesn't seem it will be needed with the new identifiers.

---
+++ mlte/spec/model.py
@@ -0,0 +1,75 @@
+"""
+mlte/spec/model.py
+
+Model implementation for the Spec artifact.
+"""
+
+from typing import Optional, List
+
+# from mlte.artifact.type import ArtifactType
+from mlte.model import BaseModel
+
+
+class SpecMetadataModel(BaseModel):
+    """Metadata related to the model."""
+
+    # TODO: do we want to keep this same metadata?
+    namespace: Optional[str] = None
+    """Namespace where this Spec is associated."""
+
+    model: Optional[str] = None
+    """The model associated to this spec."""
+
+    version: Optional[str] = None
+    """The version of the model."""
+
+    timestamp: Optional[int] = None
+    """Unix timestamp of when the creation of this spec."""
+
+
+class ConditionModel(BaseModel):
+    """A description of a condition for a property."""
+
+    name: Optional[str] = None
+    """A decriptive name for the condition, usually the method name used to call it."""
+
+    arguments: List[str] = []
+    """The arguments used when validating the condition."""
+
+    callback: Optional[str] = None
+    """A text-encoded, dilled-serialized version of the callback to execute when validating this condition."""
+
+
+class PropertyModel(BaseModel):
+    """A description of a property."""
+
+    name: Optional[str] = None
+    """A name for the property."""
+
+    repr: Optional[str] = None
+    """A string representation of the object."""
+
+    description: Optional[str] = None
+    """A general description of this property type."""
+
+    rationale: Optional[str] = None
+    """The rationale for this property being important in this situation."""
+
+    conditions: List[ConditionModel] = []
+    """A list of conditions to be validated for this property."""
+
+
+class SpecModel(BaseModel):
+    """The model implementation for the Spec artifact."""
+
+    # artifact_type: Literal[ArtifactType.SPEC]
+    """Union discriminator."""
+
+    metadata: SpecMetadataModel
+    """General metadata associated with the spec."""
+
+    properties: List[PropertyModel] = []
+    """A list of properties for this spec."""
+
+
+SpecModel.update_forward_refs()
