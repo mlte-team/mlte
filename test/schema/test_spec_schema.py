927400806b77c13b771d7be2b133608bec6b1e07
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2023-09-08 13:47:07 -0400

    Fixed and re-enabled schema tests (except for report)

--- test/schema/test_spec_schema.py
+++ test/schema/test_spec_schema.py
@@ -4,21 +4,19 @@ test/schema/test_spec_schema.py
 Unit tests for Spec schema.
 """
 
-import pytest
 
 from mlte._private.schema import validate_spec_schema
-from mlte.api import read_spec
 from mlte.property.costs import StorageCost
 from mlte.spec.spec import Spec
 from mlte.value.types.integer import Integer
 
+from ..fixture.store import store_with_context  # noqa
 
-@pytest.mark.skip("Disabled for artifact protocol development.")
-def test_instance_with_content(tmp_path):
+
+def test_instance_with_content():
     spec = Spec(
         properties={StorageCost("rationale"): {"test": Integer.less_than(3)}}
     )
-    spec.save()
 
-    doc = read_spec(f"local://{tmp_path}", "model", "0.0.1")
-    validate_spec_schema(doc)
+    doc = spec.to_model().to_json()
+    validate_spec_schema(doc["body"])
--- test/schema/test_value_schema.py
+++ test/schema/test_value_schema.py
@@ -4,53 +4,44 @@ test/schema/test_value_schema.py
 Unit tests for Value schema.
 """
 
-import pytest
-
 from mlte._private.schema import validate_value_schema
-from mlte.api import read_value
 from mlte.evidence.metadata import EvidenceMetadata, Identifier
 from mlte.value.types.integer import Integer
 from mlte.value.types.opaque import Opaque
 from mlte.value.types.real import Real
 
 
-@pytest.mark.skip("Disabled for artifact protocol development.")
-def test_real(tmp_path):
+def test_real():
     r = Real(
         EvidenceMetadata(
             measurement_type="typename", identifier=Identifier(name="id")
         ),
         3.14,
     )
-    r.save()
 
-    d = read_value(f"local://{tmp_path}", "model", "0.0.1", "id")
-    validate_value_schema(d)
+    doc = r.to_model().to_json()
+    validate_value_schema(doc["body"])
 
 
-@pytest.mark.skip("Disabled for artifact protocol development.")
-def test_integer(tmp_path):
+def test_integer():
     r = Integer(
         EvidenceMetadata(
             measurement_type="typename", identifier=Identifier(name="id")
         ),
         3,
     )
-    r.save()
 
-    d = read_value(f"local://{tmp_path}", "model", "0.0.1", "id")
-    validate_value_schema(d)
+    doc = r.to_model().to_json()
+    validate_value_schema(doc["body"])
 
 
-@pytest.mark.skip("Disabled for artifact protocol development.")
-def test_opaque(tmp_path):
+def test_opaque():
     r = Opaque(
         EvidenceMetadata(
             measurement_type="typename", identifier=Identifier(name="id")
         ),
         {"foo": "bar"},
     )
-    r.save()
 
-    d = read_value(f"local://{tmp_path}", "model", "0.0.1", "id")
-    validate_value_schema(d)
+    doc = r.to_model().to_json()
+    validate_value_schema(doc["body"])
