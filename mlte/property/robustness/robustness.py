fed0d9554d68c1d0be387df96f3f144121e262b3
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2023-09-13 14:39:11 -0400

    Minor formatting

--- mlte/property/robustness/robustness.py
+++ mlte/property/robustness/robustness.py
@@ -17,9 +17,9 @@ class Robustness(Property):
             self.__class__.__name__,
             cleantext(
                 """
-                The Robustness property  focuses on checking that the model performance does not vary with respect to noise 
-                (or small changes in the input data that do not change the data label). Robustness is evaluating model 
-                performance as something is happening during the collection of the data that will not affect a data attribute 
+                The Robustness property  focuses on checking that the model performance does not vary with respect to noise
+                (or small changes in the input data that do not change the data label). Robustness is evaluating model
+                performance as something is happening during the collection of the data that will not affect a data attribute
                 but will affect how the object may be perceived.
                 """
             ),
