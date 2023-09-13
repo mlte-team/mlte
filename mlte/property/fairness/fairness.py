ccd64b2881fbb98740daf2131d483c6ce7f4154f
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2023-09-13 13:59:21 -0400

    Added initial definitions for Fairness and Robustness

--- mlte/property/fairness/fairness.py
+++ mlte/property/fairness/fairness.py
@@ -8,7 +8,7 @@ from mlte.property import Property
 
 class Fairness(Property):
     """
-    The Fairness property reflects the cost of model storage.
+    The Fairness property ensures similar model performance across specified subpopulations, groups, or data.
     """
 
     def __init__(self, rationale: str):
@@ -17,7 +17,8 @@ class Fairness(Property):
             self.__class__.__name__,
             cleantext(
                 """
-                TODO
+                The Fairness property ensures similar model performance across specified subpopulations, groups, or data.
+                Fairness is measured by evaluating model performance on population data sub-divided by an attribute.
                 """
             ),
             rationale,
--- mlte/property/robustness/robustness.py
+++ mlte/property/robustness/robustness.py
@@ -8,7 +8,7 @@ from mlte.property import Property
 
 class Robustness(Property):
     """
-    The Robustness property reflects the cost of model storage.
+    The Robustness property focuses on checking that the model performance does not vary with respect to noise.
     """
 
     def __init__(self, rationale: str):
@@ -17,7 +17,10 @@ class Robustness(Property):
             self.__class__.__name__,
             cleantext(
                 """
-                TODO
+                The Robustness property  focuses on checking that the model performance does not vary with respect to noise 
+                (or small changes in the input data that do not change the data label). Robustness is evaluating model 
+                performance as something is happening during the collection of the data that will not affect a data attribute 
+                but will affect how the object may be perceived.
                 """
             ),
             rationale,
