4c7286fd0fff58a6c48861c717573d55e89fcc01
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2023-09-20 15:32:35 -0400

    Fixed formatting and tensorflow dep version to avoid dependency conflicts

--- mlte/property/costs/__init__.py
+++ mlte/property/costs/__init__.py
@@ -1,7 +1,13 @@
+from .predicting_compute_cost import PredictingComputeCost
+from .predicting_memory_cost import PredictingMemoryCost
 from .storage_cost import StorageCost
 from .training_compute_cost import TrainingComputeCost
 from .training_memory_cost import TrainingMemoryCost
-from .predicting_compute_cost import PredictingComputeCost
-from .predicting_memory_cost import PredictingMemoryCost
 
-__all__ = ["StorageCost", "TrainingMemoryCost", "TrainingComputeCost", "PredictingComputeCost", "PredictingMemoryCost"]
+__all__ = [
+    "StorageCost",
+    "TrainingMemoryCost",
+    "TrainingComputeCost",
+    "PredictingComputeCost",
+    "PredictingMemoryCost",
+]
