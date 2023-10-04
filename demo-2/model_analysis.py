1c5c6879e15bac658b68af789eba9e6a899a2947
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2023-10-04 10:05:17 -0400

    Internal reformatting

--- demo-2/model_analysis.py
+++ demo-2/model_analysis.py
@@ -1,6 +1,6 @@
+import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow.keras.models import model_from_json
-import matplotlib.pyplot as plt
 
 
 def load_model(model_filename: str, weights_filename: str):
@@ -23,76 +23,75 @@ def run_model(im, loaded_model):
 
 
 def read_image(filename):
-  image = tf.io.read_file(filename)
-  image = tf.io.decode_jpeg(image, channels=3)
-  image = tf.image.convert_image_dtype(image, tf.float32)
-  image = tf.image.resize_with_pad(image, target_height=224, target_width=224)
-  return image
+    image = tf.io.read_file(filename)
+    image = tf.io.decode_jpeg(image, channels=3)
+    image = tf.image.convert_image_dtype(image, tf.float32)
+    image = tf.image.resize_with_pad(image, target_height=224, target_width=224)
+    return image
 
 
 def generate_baseline_and_alphas():
-    baseline = tf.zeros(shape=(224,224,3))
-    m_steps=50
-    alphas = tf.linspace(start=0.0, stop=1.0, num=m_steps+1) # Generate m_steps intervals for integral_approximation() below.   
+    baseline = tf.zeros(shape=(224, 224, 3))
+    m_steps = 50
+    alphas = tf.linspace(
+        start=0.0, stop=1.0, num=m_steps + 1
+    )  # Generate m_steps intervals for integral_approximation() below.
     return baseline, alphas
 
 
-def interpolate_images(baseline,
-                       image,
-                       alphas):
-  alphas_x = alphas[:, tf.newaxis, tf.newaxis, tf.newaxis]
-  baseline_x = tf.expand_dims(baseline, axis=0)
-  input_x = tf.expand_dims(image, axis=0)
-  delta = input_x - baseline_x
-  images = baseline_x +  alphas_x * delta
-  return images
+def interpolate_images(baseline, image, alphas):
+    alphas_x = alphas[:, tf.newaxis, tf.newaxis, tf.newaxis]
+    baseline_x = tf.expand_dims(baseline, axis=0)
+    input_x = tf.expand_dims(image, axis=0)
+    delta = input_x - baseline_x
+    images = baseline_x + alphas_x * delta
+    return images
 
 
 def integral_approximation(gradients):
-  # riemann_trapezoidal
-  grads = (gradients[:-1] + gradients[1:]) / tf.constant(2.0)
-  int_gradients = tf.math.reduce_mean(grads, axis=0)
-  return int_gradients
+    # riemann_trapezoidal
+    grads = (gradients[:-1] + gradients[1:]) / tf.constant(2.0)
+    int_gradients = tf.math.reduce_mean(grads, axis=0)
+    return int_gradients
 
 
 def compute_gradients(loaded_model, images, target_class_idx):
-  with tf.GradientTape() as tape:
-    tape.watch(images)
-    probs = loaded_model(images)[:, target_class_idx]
-    """
+    with tf.GradientTape() as tape:
+        tape.watch(images)
+        probs = loaded_model(images)[:, target_class_idx]
+        """
     If your model does not have a softmax output layer,
     comment out the above line and un-comment the following 2 lines
     """
-    # logits = loaded_model(images)
-    # probs = tf.nn.softmax(logits, axis=-1)[:, target_class_idx]
-    
-  return tape.gradient(probs, images)
+        # logits = loaded_model(images)
+        # probs = tf.nn.softmax(logits, axis=-1)[:, target_class_idx]
+
+    return tape.gradient(probs, images)
 
 
 @tf.function
 def one_batch(baseline, image, alpha_batch, target_class_idx, loaded_model):
     # Generate interpolated inputs between baseline and input.
-    interpolated_path_input_batch = interpolate_images(baseline=baseline,
-                                                       image=image,
-                                                       alphas=alpha_batch)
+    interpolated_path_input_batch = interpolate_images(
+        baseline=baseline, image=image, alphas=alpha_batch
+    )
 
     # Compute gradients between model outputs and interpolated inputs.
-    gradient_batch = compute_gradients(loaded_model=loaded_model, 
-                                       images=interpolated_path_input_batch,
-                                       target_class_idx=target_class_idx)
+    gradient_batch = compute_gradients(
+        loaded_model=loaded_model,
+        images=interpolated_path_input_batch,
+        target_class_idx=target_class_idx,
+    )
     return gradient_batch
 
 
-def integrated_gradients(baseline,
-                         image,
-                         target_class_idx,
-                         loaded_model,
-                         m_steps=50,
-                         batch_size=32):
+def integrated_gradients(
+    baseline, image, target_class_idx, loaded_model, m_steps=50, batch_size=32
+):
     # Generate alphas.
-    alphas = tf.linspace(start=0.0, stop=1.0, num=m_steps+1)
+    alphas = tf.linspace(start=0.0, stop=1.0, num=m_steps + 1)
 
-    # Collect gradients.    
+    # Collect gradients.
     gradient_batches = []
 
     # Iterate alphas range and batch computation for speed, memory efficiency, and scaling to larger m_steps.
@@ -101,7 +100,9 @@ def integrated_gradients(baseline,
         to = tf.minimum(from_ + batch_size, len(alphas))
         alpha_batch = alphas[from_:to]
 
-        gradient_batch = one_batch(baseline, image, alpha_batch, target_class_idx, loaded_model)
+        gradient_batch = one_batch(
+            baseline, image, alpha_batch, target_class_idx, loaded_model
+        )
         gradient_batches.append(gradient_batch)
 
     # Concatenate path gradients together row-wise into single tensor.
@@ -115,19 +116,22 @@ def integrated_gradients(baseline,
     return gradients
 
 
-def plot_img_attributions(baseline,
-                          image,
-                          target_class_idx,
-                          loaded_model,
-                          m_steps=50,
-                          cmap=None,
-                          overlay_alpha=0.4):
-
-    attributions = integrated_gradients(baseline=baseline,
-                                        image=image,
-                                        target_class_idx=target_class_idx,
-                                        loaded_model=loaded_model,
-                                        m_steps=m_steps)
+def plot_img_attributions(
+    baseline,
+    image,
+    target_class_idx,
+    loaded_model,
+    m_steps=50,
+    cmap=None,
+    overlay_alpha=0.4,
+):
+    attributions = integrated_gradients(
+        baseline=baseline,
+        image=image,
+        target_class_idx=target_class_idx,
+        loaded_model=loaded_model,
+        m_steps=m_steps,
+    )
 
     # Sum of the attributions across color channels for visualization.
     # The attribution mask shape is a grayscale image with height and width
@@ -136,22 +140,22 @@ def plot_img_attributions(baseline,
 
     fig, axs = plt.subplots(nrows=2, ncols=2, squeeze=False, figsize=(8, 8))
 
-    axs[0, 0].set_title('Baseline image')
+    axs[0, 0].set_title("Baseline image")
     axs[0, 0].imshow(baseline)
-    axs[0, 0].axis('off')
+    axs[0, 0].axis("off")
 
-    axs[0, 1].set_title('Original image')
+    axs[0, 1].set_title("Original image")
     axs[0, 1].imshow(image)
-    axs[0, 1].axis('off')
+    axs[0, 1].axis("off")
 
-    axs[1, 0].set_title('Attribution mask')
+    axs[1, 0].set_title("Attribution mask")
     axs[1, 0].imshow(attribution_mask, cmap=cmap)
-    axs[1, 0].axis('off')
+    axs[1, 0].axis("off")
 
-    axs[1, 1].set_title('Overlay')
+    axs[1, 1].set_title("Overlay")
     axs[1, 1].imshow(attribution_mask, cmap=cmap)
     axs[1, 1].imshow(image, alpha=overlay_alpha)
-    axs[1, 1].axis('off')
+    axs[1, 1].axis("off")
 
     plt.tight_layout()
     return fig
--- demo-2/model_predict.py
+++ demo-2/model_predict.py
@@ -1,9 +1,8 @@
-import time
-import os
 import argparse
-from resource import *
+import os
 import sys
-
+import time
+from resource import *
 
 """
 This script gets performance metrics (elapsed time and memory consumption) for running inference on a model.
@@ -21,7 +20,9 @@ parser.add_argument(
     required=True,
 )
 # --model file example: model_f3_a.json
-parser.add_argument("--model", help="The json formatted model file.", required=True)
+parser.add_argument(
+    "--model", help="The json formatted model file.", required=True
+)
 # --weights file example: model_f_a.h5
 parser.add_argument(
     "--weights", help="The file that contains the model weights.", required=True
@@ -92,7 +93,11 @@ for image in dataset:
 avg_elapsed_time = total_elapsed_time / num_samples
 avg_inference_memory = total_inference_memory / num_samples
 print("\n--- STATISTICS ---")
-print("Average elapsed time per inference: {0:.5f} seconds".format(avg_elapsed_time))
+print(
+    "Average elapsed time per inference: {0:.5f} seconds".format(
+        avg_elapsed_time
+    )
+)
 print(
     "Average memory used per inference: {0:.5f} {1}.".format(
         avg_inference_memory, r_mem_units_str
