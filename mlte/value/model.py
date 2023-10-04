cbfd19b32d3861a1d164f8b39d060f566ef26de2
turingcompl33t <kdotterrer@gmail.com>
2023-10-04 06:08:52 -0400

    use enumeration values in value models

--- mlte/value/model.py
+++ mlte/value/model.py
@@ -59,6 +59,9 @@ class IntegerValueModel(BaseModel):
     integer: int
     """The encapsulated value."""
 
+    class Config:
+        use_enum_values = True
+
 
 class RealValueModel(BaseModel):
     """The model implementation for MLTE real values."""
@@ -68,6 +71,9 @@ class RealValueModel(BaseModel):
 
     real: float
     """The encapsulated value."""
+
+    class Config:
+        use_enum_values = True
 
 
 class OpaqueValueModel(BaseModel):
@@ -79,6 +85,9 @@ class OpaqueValueModel(BaseModel):
     data: Dict[str, Any]
     """Encapsulated, opaque data."""
 
+    class Config:
+        use_enum_values = True
+
 
 class ImageValueModel(BaseModel):
     """The model implementation for MLTE image values."""
@@ -88,6 +97,9 @@ class ImageValueModel(BaseModel):
 
     data: str
     """The image data as base64-encoded string."""
+
+    class Config:
+        use_enum_values = True
 
 
 ValueModel.model_rebuild()
