d197ab1615f949dad426ab7f67346b6af62dce78
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2024-03-15 16:37:27 -0400

    Added password hashing test

--- test/web/store/auth/test_auth.py
+++ test/web/store/auth/test_auth.py
@@ -5,7 +5,7 @@ Test the authentication operations
 """
 
 
-from mlte.web.store.api.auth import token
+from mlte.web.store.api.auth import authentication, token
 
 
 def test_token_encode_decode() -> None:
@@ -16,3 +16,18 @@ def test_token_encode_decode() -> None:
     decoded_username = token.decode_user_token(new_token)
 
     assert username == decoded_username
+
+
+# TODO: test expiration claim of a token.
+
+
+def test_hash_verification() -> None:
+    """Checks that password can be hashed and verified."""
+    password = "secret"
+
+    hashed_pass = authentication.get_password_hash(password)
+    verification_success = authentication._verify_password(
+        password, hashed_pass
+    )
+
+    assert verification_success
