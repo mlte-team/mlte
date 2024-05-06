7ae819571c64db9310217b574ae94c6990706620
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2024-05-06 17:03:20 -0400

    Added roles, permissions and group models for users

--- mlte/user/model.py
+++ mlte/user/model.py
@@ -3,10 +3,22 @@ mlte/user/model.py
 
 Model implementation for a User.
 """
+from __future__ import annotations
 
-from typing import Optional
+from enum import Enum
+from typing import List, Optional
 
 from mlte.model import BaseModel
+
+
+class RoleType(str, Enum):
+    """Roles for users."""
+
+    ADMIN = "admin"
+    """An admin role, access to everything."""
+
+    REGULAR = "regular"
+    """A role with access to artifacts only."""
 
 
 class BasicUser(BaseModel):
@@ -24,6 +36,12 @@ class BasicUser(BaseModel):
     disabled: bool = False
     """Whether the user is disabled."""
 
+    role: RoleType = RoleType.REGULAR
+    """The role associated to the user."""
+
+    groups: List[Group] = []
+    """The groups the user is in."""
+
 
 class User(BasicUser):
     """User with additional information only used locally when stored."""
@@ -37,3 +55,33 @@ class UserCreate(BasicUser):
 
     password: str
     """The plain password of the user."""
+
+
+class PermissionType(str, Enum):
+    """Enumerates all supported permission types."""
+
+    READ = "read"
+    """Permission to read a specific model and its artifacts."""
+
+    WRITE = "write"
+    """Permission to write artifacts for a specific model."""
+
+
+class Permission(BaseModel):
+    """Permissions for specific artifacts."""
+
+    permission_type: PermissionType = PermissionType.READ
+    """The type of permisison granted."""
+
+    model_identifier: str
+    """The model to give permissions to."""
+
+
+class Group(BaseModel):
+    """A user group to which permissions are associated."""
+
+    name: str
+    """The name of the group."""
+
+    permissions: List[PermissionType] = []
+    """The permissions associated to the group."""
