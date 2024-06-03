766138b50522dab1511ac24a792d142e6861b230
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2024-06-03 11:21:45 -0400

    Frontend: fixing bug, settings were not being properly passed. Also added proper exit codes.

--- mlte/frontend/__init__.py
+++ mlte/frontend/__init__.py
@@ -4,10 +4,22 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
-from mlte.frontend.config import settings
+# Application exit codes
+EXIT_SUCCESS = 0
+EXIT_FAILURE = 1
 
 
-def run_frontend():
+def run_frontend(
+    host: str,
+    port: int,
+) -> int:
+    """
+    Run the frontend UI application.
+    :param host: The application host
+    :param port: The application port
+    :return: Return code
+    """
+
     app = FastAPI()
     static_dir = (
         os.path.dirname(os.path.abspath(__file__)) + "/nuxt-app/.output/public"
@@ -17,8 +29,10 @@ def run_frontend():
         app.mount(
             "/", StaticFiles(directory=static_dir, html=True), name="static"
         )
-        uvicorn.run(app, host=settings.APP_HOST, port=int(settings.APP_PORT))
+        uvicorn.run(app, host=host, port=port)
+        return EXIT_SUCCESS
     else:
         print(
             "Unable to run frontend. This is likely due to an issue with this distribution of the package."
         )
+        return EXIT_FAILURE
