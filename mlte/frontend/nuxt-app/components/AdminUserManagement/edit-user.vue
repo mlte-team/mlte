c838c9ef44be8c99ee389f984f63203ff0dab16b
Alex Derr <aderr@sei.cmu.edu>
2024-06-10 14:28:03 -0400

    Better user management page
    - Separate form to edit user data after list
    - Ability to change password
    - Ability to edit roles
    - Ability to edit groups
    - This will be admin only eventually, currently open to all users

---
+++ mlte/frontend/nuxt-app/components/AdminUserManagement/edit-user.vue
@@ -0,0 +1,248 @@
+<template>
+  <div v-if="!newUserFlag">
+    <div class="flex-container">
+      <h1 class="section-header">{{ modelValue.username }}</h1>
+      <div
+        class="centered-container"
+        style="vertical-align: bottom; padding-left: 33ch;"
+      >
+        <div v-if="!resetPasswordFlag">
+          <UsaButton
+            class="secondary-button"
+            @click="enablePasswordReset"
+          >
+            Reset Password
+          </UsaButton>
+        </div>
+        <div v-if="resetPasswordFlag">
+          <UsaButton
+            class="secondary-button"
+            @click="disablePasswordReset"
+          >
+            Cancel Reset
+          </UsaButton>
+        </div>
+      </div>
+    </div>
+    <div v-if="resetPasswordFlag">
+      <UsaTextInput
+        v-model="modelValue.password"
+        :error="formErrors.password"
+        type="password"
+      >
+        <template #label>
+          New Password
+        </template>
+        <template #error-message>
+          Password is required
+        </template>
+      </UsaTextInput>
+      <UsaTextInput
+        v-model="confirmPassword"
+        :error="formErrors.confirmPassword"
+        type="password"
+      >
+      <template #label>
+        Confirm New Password
+      </template>
+      <template #error-message>
+        Passwords to not match
+      </template>
+      </UsaTextInput>
+    </div>
+  </div>
+  <div v-if="newUserFlag">
+    <UsaTextInput
+      v-model="modelValue.username"
+      :error="formErrors.username"
+    >
+      <template #label>
+        Username
+      </template>
+      <template #error-message>
+        Username is required
+      </template>
+    </UsaTextInput>
+
+    <UsaTextInput
+      v-model="modelValue.password" 
+      :error="formErrors.password"
+      type="password"
+    >
+      <template #label>
+        Password
+      </template>
+      <template #error-message>
+        Password is required
+      </template>
+    </UsaTextInput>
+  </div>
+  
+  <UsaTextInput v-model="modelValue.email">
+    <template #label>
+      Email
+    </template>
+  </UsaTextInput>
+
+  <UsaTextInput v-model="modelValue.full_name">
+    <template #label>
+      Full Name
+    </template>
+  </UsaTextInput>
+
+  <UsaCheckbox v-model="modelValue.disabled" label="Disabled" />
+
+  <UsaSelect 
+    v-model="modelValue.role"
+    :options="roleOptions"
+    :error="formErrors.role"
+    @change="formErrors.role = false"
+  >
+    <template #label>
+      Role
+    </template>
+    <template #error-message>
+      A role must be selected
+    </template>
+  </UsaSelect>
+
+  <label class="usa-label">Groups</label>
+  <div v-for="groupOption in groupOptions" :key="groupOption.name">
+    <UsaCheckbox
+      v-model="groupOption.selected"
+      :label="groupOption.name"
+      @update:modelValue="groupChange(groupOption.selected, groupOption.name)"
+    />
+  </div>
+
+  <div class="submit-footer">
+    <UsaButton class="primary-button" @click="$emit('cancel')">
+      Cancel
+    </UsaButton>
+    <UsaButton class="primary-button" @click="submit()"> Save </UsaButton>
+  </div>
+</template>
+
+<script setup lang="ts">
+const config = useRuntimeConfig();
+const token = useCookie("token");
+
+const emit = defineEmits([
+  "cancel",
+  "submit",
+  "updateUserGroups"
+]);
+
+const props = defineProps({
+  modelValue: {
+    required: true,
+    default: {},
+  },
+  newUserFlag: {
+    required: true,
+    default: false,
+  },
+});
+
+const resetPasswordFlag = ref(false);
+const confirmPassword = ref("");
+const roleOptions = ref([
+  {value: "admin", text: "admin"},
+  {value: "regular", text: "regular"}
+]);
+const formErrors = ref({
+  username: false,
+  role: false,
+  password: false,
+  confirmPassword: false,
+})
+const groupOptions = ref<{name: string; selected: boolean}[]>([]);
+const {data: groupList} = await useFetch<string[]>(
+  config.public.apiPath + "/group",
+  {
+    method: "GET",
+    headers: {
+      Authorization: "Bearer " + token.value,
+    },
+  },
+);
+if (groupList.value) {
+  groupList.value.forEach((groupName: string) => {
+    groupOptions.value.push({ name: groupName, selected: false });
+  });
+}
+
+groupOptions.value.forEach((groupOption) => {
+  if(props.modelValue.groups.find(x => x.name === groupOption.name)){
+    groupOption.selected = true;
+  }
+});
+
+function enablePasswordReset(){
+  resetPasswordFlag.value = true;
+  props.modelValue.password = "";
+}
+
+function disablePasswordReset(){
+  resetPasswordFlag.value = false;
+  delete props.modelValue.password;
+}
+
+function groupChange(selected: boolean, groupName: string){
+  let tempGroupList = props.modelValue.groups;
+
+  if(selected){
+    tempGroupList.push({name: groupName})
+  }
+  else{
+    let index = props.modelValue.groups.indexOf({name: groupName});
+    tempGroupList.splice(index, 1);
+  }
+
+  emit("updateUserGroups", tempGroupList);  
+}
+
+function submit(){
+  resetFormErrors();
+  let submitError = false;
+
+  if(props.newUserFlag){
+    if(props.modelValue.username.trim() === ""){
+      formErrors.value.username = true;
+      submitError = true;
+    };
+
+    if(props.modelValue.password.trim() === ""){
+      formErrors.value.password = true;
+      submitError = true;
+    };
+  }
+
+  if(resetPasswordFlag.value){
+    if(props.modelValue.password.trim() === ""){
+      formErrors.value.password = true;
+      submitError = true;
+    }
+    if(props.modelValue.password != confirmPassword.value){
+      formErrors.value.confirmPassword = true;
+      submitError = true;
+    }
+  }
+
+  if(props.modelValue.role.trim() === ""){
+    formErrors.value.role = true;
+    submitError = true;
+  };
+
+  if(!submitError){
+    emit("submit", props.modelValue);
+  };
+}
+
+function resetFormErrors(){
+  formErrors.value.username = false;
+  formErrors.value.role = false;
+  formErrors.value.password = false;
+  formErrors.value.confirmPassword = false;
+}
+</script>
---
+++ mlte/frontend/nuxt-app/components/AdminUserManagement/user-list.vue
@@ -0,0 +1,68 @@
+<template>
+  <table class="table usa-table usa-table--borderless">
+    <thead>
+      <tr>
+        <th data-sortable scope="col" role="columnheader">Username</th>
+        <th data-sortable scope="col" role="columnheader">Email</th>
+        <th data-sortable scope="col" role="columnheader">Full Name</th>
+        <th data-sortable scope="col" role="columnheader">Disabled</th>
+        <th scope="col" role="columnheader">Actions</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(user, index) in modelValue" :key="index">
+        <td>
+          {{ user.username }}
+        </td>
+        <td>
+          {{ user.email }}
+        </td>
+        <td>
+          {{ user.full_name }}
+        </td>
+        <td>
+          <div class="centered-container" style="margin-bottom: 1.5em">
+            <UsaCheckbox v-model="user.disabled" disabled />
+          </div>
+        </td>
+        <td>
+          <UsaButton
+            class="secondary-button"
+            @click="$emit('editUser', user)"
+          >
+            Edit
+          </UsaButton>
+
+          <UsaButton
+            class="usa-button usa-button--secondary"
+            @click="$emit('deleteUser', user.username)"
+          >
+            Delete
+          </UsaButton>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+  <UsaButton
+    class="primary-button"
+    @click="$emit('addUser')"
+  >
+    Add User
+  </UsaButton>
+</template>
+
+<script setup lang="ts">
+const emit = defineEmits([
+  "addUser",
+  "editUser",
+  "deleteUser"
+])
+
+const props = defineProps({
+  modelValue: {
+    type: Array,
+    required: true,
+    default: []
+  }
+})
+</script>
