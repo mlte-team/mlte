bfc4c33a33fa58a050a836709cfd02889c23f3e0
Alex Derr <aderr@sei.cmu.edu>
2024-07-30 16:31:43 -0400

    Fix bug if last model was deleted
    - If last model is deleted, cookie will properly reset

--- mlte/frontend/nuxt-app/pages/index.vue
+++ mlte/frontend/nuxt-app/pages/index.vue
@@ -317,10 +317,11 @@ const values = ref<
 >([]);
 
 await populateModelVersionLists();
-if (modelOptions.value !== null && modelOptions.value.length > 0) {
+if (modelOptions.value !== null) {
   const modelList = modelOptions.value.map((model) => {
     return model.value;
   });
+
   if (!modelList.includes(selectedModel.value)) {
     selectedModel.value = "";
     selectedVersion.value = "";
