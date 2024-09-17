7665b491eb61f718e83ffd5a45a25f459b5f5ce9
Alex Derr <aderr@sei.cmu.edu>
2024-09-17 15:38:47 -0400

    Add more columns to test catalog list and better search

--- mlte/frontend/nuxt-app/components/TestCatalog/entry-list.vue
+++ mlte/frontend/nuxt-app/components/TestCatalog/entry-list.vue
@@ -3,12 +3,31 @@
     <thead>
       <tr>
         <th data-sortable scope="col" role="columnheader">Identifier</th>
+        <th data-sortable scope="col" role="columnheader">Catalog</th>
+        <th data-sortable scope="col" role="columnheader">Tags</th>
+        <th data-sortable scope="col" role="columnheader">Property Category</th>
+        <th data-sortable scope="col" role="columnheader">Code Type</th>
         <th data-sortable scope="col" role="columnheader">Actions</th>
       </tr>
     </thead>
     <tr v-for="(entry, entryIndex) in modelValue" :key="entryIndex">
       <td>
         {{ entry.header.identifier }}
+      </td>
+      <td>
+        {{ entry.header.catalog_id }}
+      </td>
+      <td>
+        <span v-for="(tag, tagIndex) in entry.problem_type">
+          <span>{{ tag }}</span>
+          <span v-if="tagIndex + 1 < entry.problem_type.length">, </span>
+        </span>
+      </td>
+      <td>
+        {{ entry.property_category }}
+      </td>
+      <td>
+        {{ entry.code_type }}
       </td>
       <td>
         <UsaButton class="secondary-button" @click="$emit('editEntry', entry)">
