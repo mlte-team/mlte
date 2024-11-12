9d6a6c3ea3d45643a6ad8c2357f2405ebbb63bcb
aderr <aderr@sei.cmu.edu>
2024-11-12 11:33:21 -0500

    Cookie updates
    - No longer use secure cookie for token to make simple server setups work without requiring https
    - Reset all user cookies on logout
    - Add maxAge to all user cookies

--- mlte/frontend/nuxt-app/composables/auth.ts
+++ mlte/frontend/nuxt-app/composables/auth.ts
@@ -1,9 +1,13 @@
 export function confirmLogout() {
   const token = useCookie("token");
+  const user = useCookie("user");
+  const userRole = useCookie("userRole")
   if (
     confirm("Are you sure you want to logout? All unsaved changes wll be lost.")
   ) {
     token.value = undefined;
+    user.value = undefined;
+    userRole.value = undefined;
     navigateTo("/login");
   }
 }
