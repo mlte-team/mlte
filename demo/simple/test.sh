88755172869065a4c89661cd760e532d3fc0c240
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2024-12-13 19:39:09 -0500

    Demo tests: removed curr folder duplication for more maintainability

--- demo/simple/test.sh
+++ demo/simple/test.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
+CURR_FOLDER="./demo/simple"
 
 # Remove everything from temp stores to avoid outdated data.
-rm -r ./store/models
-mkdir -p ./store/models
+rm -r ${CURR_FOLDER}/store/models
+mkdir -p ${CURR_FOLDER}/store/models
 
-poetry run pytest --nbmake ./demo/simple/negotiation.ipynb
-poetry run pytest --nbmake ./demo/simple/requirements.ipynb
-poetry run pytest --nbmake ./demo/simple/evidence.ipynb
-poetry run pytest --nbmake ./demo/simple/report.ipynb
+poetry run pytest --nbmake ${CURR_FOLDER}/negotiation.ipynb
+poetry run pytest --nbmake ${CURR_FOLDER}/requirements.ipynb
+poetry run pytest --nbmake ${CURR_FOLDER}/evidence.ipynb
+poetry run pytest --nbmake ${CURR_FOLDER}/report.ipynb
