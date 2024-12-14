f6b08f0bdb4b5a1398d056d55145f5a9bb56ef02
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2024-12-13 19:39:09 -0500

    Demo tests: removed curr folder duplication for more maintainability
    # Conflicts:
    #	demo/scenarios/test.sh
    #	demo/simple/test.sh

--- demo/simple/test.sh
+++ demo/simple/test.sh
@@ -1,4 +1,14 @@
-poetry run pytest --nbmake ./demo/simple/1_negotiation.ipynb
-poetry run pytest --nbmake ./demo/simple/2_requirements.ipynb
-poetry run pytest --nbmake ./demo/simple/3_evidence.ipynb
-poetry run pytest --nbmake ./demo/simple/4_report.ipynb
+#!/usr/bin/env bash
+CURR_FOLDER="./demo/simple"
+
+# Remove everything from temp stores to avoid outdated data.
+rm -r ${CURR_FOLDER}/store/models
+mkdir -p ${CURR_FOLDER}/store/models
+
+# Copy sample negotiation card to store.
+source copy_nc.sh
+
+poetry run pytest --nbmake ${CURR_FOLDER}/1_negotiation.ipynb
+poetry run pytest --nbmake ${CURR_FOLDER}/2_requirements.ipynb
+poetry run pytest --nbmake ${CURR_FOLDER}/3_evidence.ipynb
+poetry run pytest --nbmake ${CURR_FOLDER}/4_report.ipynb
