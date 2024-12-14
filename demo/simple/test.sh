42d688e601c4597c4e2f7441cebe40185de85ad4
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2024-12-13 19:36:23 -0500

    Demo: added temp store cleanup to avoid errors with outdated data

--- demo/simple/test.sh
+++ demo/simple/test.sh
@@ -1,3 +1,9 @@
+#!/usr/bin/env bash
+
+# Remove everything from temp stores to avoid outdated data.
+rm -r ./store/models
+mkdir -p ./store/models
+
 poetry run pytest --nbmake ./demo/simple/negotiation.ipynb
 poetry run pytest --nbmake ./demo/simple/requirements.ipynb
 poetry run pytest --nbmake ./demo/simple/evidence.ipynb
