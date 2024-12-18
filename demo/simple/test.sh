a7bd2407ec92c2448ca436f8d21323a52ff358b7
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2024-12-18 15:26:50 -0500

    Revert "Demo tests: removed curr folder duplication for more maintainability"
    This reverts commit 88755172869065a4c89661cd760e532d3fc0c240.

--- demo/simple/test.sh
+++ demo/simple/test.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
-CURR_FOLDER="./demo/simple"
 
 # Remove everything from temp stores to avoid outdated data.
-rm -r ${CURR_FOLDER}/store/models
-mkdir -p ${CURR_FOLDER}/store/models
+rm -r ./store/models
+mkdir -p ./store/models
 
-poetry run pytest --nbmake ${CURR_FOLDER}/negotiation.ipynb
-poetry run pytest --nbmake ${CURR_FOLDER}/requirements.ipynb
-poetry run pytest --nbmake ${CURR_FOLDER}/evidence.ipynb
-poetry run pytest --nbmake ${CURR_FOLDER}/report.ipynb
+poetry run pytest --nbmake ./demo/simple/negotiation.ipynb
+poetry run pytest --nbmake ./demo/simple/requirements.ipynb
+poetry run pytest --nbmake ./demo/simple/evidence.ipynb
+poetry run pytest --nbmake ./demo/simple/report.ipynb
