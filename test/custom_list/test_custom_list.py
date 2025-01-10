e471ecf87222c57f5e507bec74a2f015f04a3176
Alex Derr <aderr@sei.cmu.edu>
2025-01-10 15:08:57 -0500

    Serialization tests for CustomList and CustomListEntry

---
+++ test/custom_list/test_custom_list.py
@@ -0,0 +1,45 @@
+"""
+test/custom_list/test_model.py
+
+Unit tests for custom_list and custom_list_entry model
+"""
+
+from __future__ import annotations
+
+from mlte.custom_list.model import CustomList, CustomListEntry
+
+
+# -----------------------------------------------------------------------------
+# CustomList
+# -----------------------------------------------------------------------------
+
+
+def test_custom_list() -> None:
+    """A custom list can be serialized and dezerialized."""
+    objects = [
+        CustomList(
+            name="qa_categories",
+            entries=[]
+        )
+    ]
+    for object in objects:
+        s = object.to_json()
+        d = CustomList.from_json(s)
+        assert d == object
+
+# -----------------------------------------------------------------------------
+# CustomListEntry
+# -----------------------------------------------------------------------------
+
+def test_custom_list_entry() -> None:
+    """A custom list entry can be serialized and dezerialized."""
+    objects = [
+        CustomListEntry(
+            name="Test entry",
+            description="Test description"
+        )
+    ]
+    for object in objects:
+        s = object.to_json()
+        d = CustomListEntry.from_json(s)
+        assert d == object
