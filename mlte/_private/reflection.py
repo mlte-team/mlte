a49b031d9b91a286e8439fff119caff0df8319da
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2025-01-30 12:40:50 -0500

    Reflection: refactored get_lambda_code function to be more robust, and added test cases for it

--- mlte/_private/reflection.py
+++ mlte/_private/reflection.py
@@ -1,6 +1,10 @@
+import ast
 import importlib
 import inspect
+import re
 from typing import Any, Type
+
+import astunparse
 
 
 def load_class(class_path: str) -> Type[Any]:
@@ -27,9 +31,62 @@ def load_class(class_path: str) -> Type[Any]:
     return class_type
 
 
-def get_lambda_code(lambda_var: str, lambda_expression: Any) -> str:
+def get_lambda_code1(lambda_var: str, lambda_expression: Any) -> str:
     """Returns the code for a given lambda expression as a string."""
     code_string = inspect.getsource(lambda_expression).lstrip()
     start = code_string.find(f"{lambda_var}=") + len(f"{lambda_var}=")
     end = code_string.find(",", start)
     return code_string[start:end]
+
+
+def get_lambda_code(lambda_func: Any, lambda_pos: int = 0):
+    """
+    Gets the source code of a lambda function as a string.
+
+    :param lambda_func: The lambda function to inspect.
+    :param lambda_pos: If there are multiple lambdas in the line, which one to get. Defaults to 0, first one.
+    :returns: A string containing the source code of the lambda function,
+        or None if the input is not a lambda function or if the source
+        code cannot be retrieved.
+    """
+
+    # Get source code lines where lambda was defined.
+    try:
+        code_string = inspect.getsource(lambda_func).strip()
+    except (TypeError, OSError) as e:
+        print(f"Error getting source lines: {e}")
+        return None
+
+    # Create visitor that will parse the line and find the lambda.
+    class LambdaGetter(ast.NodeTransformer):
+        def __init__(self):
+            super().__init__()
+            self.lambda_sources: list[str] = []
+
+        def visit_Lambda(self, node: Any):
+            self.lambda_sources.append(astunparse.unparse(node).strip()[1:-1])
+
+        def get(self, code_string: str):
+            try:
+                tree = ast.parse(code_string)
+                self.visit(tree)
+            except SyntaxError as e:
+                print(f"Error parsing lambda: {e}")
+
+                # If we got an syntax error due to the source code line where
+                # the lambda was defined not being a fully parseable line,
+                # get the lambda plus any code after it (better than nothing).
+                match = re.search(r"\b(?:lambda)\b.*?:", code_string)
+                if match:
+                    lambda_start = match.start()
+                    lambda_body = code_string[lambda_start:]
+                    self.lambda_sources.append(lambda_body)
+
+            return self.lambda_sources
+
+    lambdas = LambdaGetter().get(code_string)
+
+    if len(lambdas) >= 1:
+        return lambdas[lambda_pos]
+    else:
+        return None
