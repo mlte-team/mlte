1f2fa17e68cd2e1f3b3a3f6305362a6f3d55f5f9
Sebastian Echeverria <secheverria@sei.cmu.edu>
2025-03-10 14:17:34 -0400

    Fixed demo test runners

--- demo/simple/test.sh
+++ demo/simple/test.sh
@@ -6,6 +6,6 @@ rm -r ${CURR_FOLDER}/store/models
 mkdir -p ${CURR_FOLDER}/store/models
 
 poetry run pytest --nbmake ${CURR_FOLDER}/1_negotiation.ipynb
-poetry run pytest --nbmake ${CURR_FOLDER}/2_requirements.ipynb
+poetry run pytest --nbmake ${CURR_FOLDER}/2_test_suite.ipynb
 poetry run pytest --nbmake ${CURR_FOLDER}/3_evidence.ipynb
 poetry run pytest --nbmake ${CURR_FOLDER}/4_report.ipynb
