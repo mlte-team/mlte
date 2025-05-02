d86bacd96a6dbb5bc5713fca7d62e21cc00ed7aa
Sebastian Echeverria <secheverria@sei.cmu.edu>
2025-05-02 15:01:07 -0400

    Build: adding comments, making simpler and more robust

--- build.sh
+++ build.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
-rm -r dist
+
+# Stop on error.
+set -e
+
+# Cleanup.
+rm -r -f dist
 mkdir dist
 
-cd mlte/frontend/nuxt-app && npm run build
-cd ../../../
+# Build frontend as static website.
+(cd mlte/frontend/nuxt-app && npm run build)
+
+# Validate pyproject.toml and lock file for consistency.
 poetry check
+
+# Do the actual sdist and wheel creation.
 poetry build
 
+# Check if the description/readme in the created packages are ok for PyPi.
 poetry run twine check dist/*
