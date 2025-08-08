79b86e2cfa09322cdbcdb1ace119661c054f6c4f
Sebastian Echeverria <secheverria@sei.cmu.edu>
2025-08-08 18:48:10 -0400

    Demo: Updated demo card with new level field

--- demo/scenarios/copy_nc.sh
+++ demo/scenarios/copy_nc.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 MODEL="OxfordFlower"
-VERSION="0.0.1"
 
 # Copy sample negotiation card to our working store space. Create folders if needed.
-mkdir -p store/models/$MODEL/$VERSION
-cp ../sample_store/models/$MODEL/$VERSION/*.json ./store/models/$MODEL/$VERSION
+mkdir -p store/models/$MODEL
+cp ../sample_store/models/$MODEL/*.json ./store/models/$MODEL
