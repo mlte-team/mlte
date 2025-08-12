7a230e1ad3673592e3a6dde62664d94af58b18c9
Sebastian Echeverria <secheverria@sei.cmu.edu>
2025-08-11 22:58:25 -0400

    Demo: changing back so default card is properly stored at model level

--- demo/scenarios/copy_nc.sh
+++ demo/scenarios/copy_nc.sh
@@ -5,4 +5,4 @@ VERSION="0.0.1"
 
 # Copy sample negotiation card to our working store space. Create folders if needed.
 mkdir -p store/models/$MODEL/$VERSION
-cp ../sample_store/models/$MODEL/$VERSION/*.json ./store/models/$MODEL/$VERSION
+cp ../sample_store/models/$MODEL/*.json ./store/models/$MODEL
