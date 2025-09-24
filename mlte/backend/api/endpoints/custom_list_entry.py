ef1531d2cd5f2fe15edfb66fa950806b4f59e1d1
Alex Derr <aderr@sei.cmu.edu>
2025-09-24 11:06:06 -0400

    Add conversions to CustomListName enum

--- mlte/backend/api/endpoints/custom_list_entry.py
+++ mlte/backend/api/endpoints/custom_list_entry.py
@@ -9,7 +9,7 @@ import mlte.store.error as errors
 from mlte.backend.api.auth.authorization import AuthorizedUser
 from mlte.backend.api.error_handlers import raise_http_internal_error
 from mlte.backend.core import state_stores
-from mlte.custom_list.custom_list_names import CustomListName
+from mlte.custom_list.custom_list_names import CustomListName, CustomListNameDict
 from mlte.custom_list.model import CustomListEntryModel
 from mlte.custom_list.custom_list_names import CustomListParentMappings
 
@@ -19,7 +19,7 @@ router = APIRouter()
 @router.post("/{custom_list_id}/entry")
 def create_custom_list_entry(
     *,
-    custom_list_id: CustomListName,
+    custom_list_id: str,
     entry: CustomListEntryModel,
     current_user: AuthorizedUser,
 ) -> CustomListEntryModel:
@@ -31,7 +31,7 @@ def create_custom_list_entry(
     with state_stores.custom_list_stores_session() as custom_list_store:
         try:
             return custom_list_store.custom_list_entry_mapper.create(
-                entry, custom_list_id
+                entry, CustomListName(custom_list_id)
             )
         except errors.ErrorNotFound as e:
             raise HTTPException(status_code=codes.NOT_FOUND, detail=f"{e}")
@@ -46,7 +46,7 @@ def create_custom_list_entry(
 @router.get("/{custom_list_id}/entry/{custom_list_entry_id}")
 def read_custom_list_entry(
     *,
-    custom_list_id: CustomListName,
+    custom_list_id: str,
     custom_list_entry_id: str,
     current_user: AuthorizedUser,
 ) -> CustomListEntryModel:
@@ -58,7 +58,7 @@ def read_custom_list_entry(
     with state_stores.custom_list_stores_session() as custom_list_store:
         try:
             return custom_list_store.custom_list_entry_mapper.read(
-                custom_list_entry_id, custom_list_id
+                custom_list_entry_id, CustomListName(custom_list_id)
             )
         except errors.ErrorNotFound as e:
             raise HTTPException(
@@ -83,7 +83,7 @@ def list_custom_lists(
 @router.get("/{custom_list_id}")
 def list_custom_list_details(
     *,
-    custom_list_id: CustomListName,
+    custom_list_id: str,
     current_user: AuthorizedUser,
 ) -> List[CustomListEntryModel]:
     """
@@ -94,7 +94,7 @@ def list_custom_list_details(
     with state_stores.custom_list_stores_session() as custom_list_store:
         try:
             return custom_list_store.custom_list_entry_mapper.list_details(
-                custom_list_id
+                CustomListName(custom_list_id)
             )
         except Exception as e:
             raise_http_internal_error(e)
@@ -103,7 +103,7 @@ def list_custom_list_details(
 @router.put("/{custom_list_id}/entry")
 def edit_custom_list_entry(
     *,
-    custom_list_id: CustomListName,
+    custom_list_id: str,
     entry: CustomListEntryModel,
     current_user: AuthorizedUser,
 ) -> CustomListEntryModel:
@@ -115,7 +115,7 @@ def edit_custom_list_entry(
     with state_stores.custom_list_stores_session() as custom_list_store:
         try:
             return custom_list_store.custom_list_entry_mapper.edit(
-                entry, custom_list_id
+                entry, CustomListName(custom_list_id)
             )
         except errors.ErrorNotFound as e:
             raise HTTPException(status_code=codes.NOT_FOUND, detail=f"{e}")
@@ -126,7 +126,7 @@ def edit_custom_list_entry(
 @router.delete("/{custom_list_id}/entry/{custom_list_entry_id}")
 def delete_custom_list_entry(
     *,
-    custom_list_id: CustomListName,
+    custom_list_id: str,
     custom_list_entry_id: str,
     current_user: AuthorizedUser,
 ) -> CustomListEntryModel:
@@ -138,7 +138,7 @@ def delete_custom_list_entry(
     with state_stores.custom_list_stores_session() as custom_list_store:
         try:
             return custom_list_store.custom_list_entry_mapper.delete(
-                custom_list_entry_id, custom_list_id
+                custom_list_entry_id, CustomListName(custom_list_id)
             )
         except errors.ErrorNotFound as e:
             raise HTTPException(
@@ -147,10 +147,11 @@ def delete_custom_list_entry(
         except Exception as e:
             raise_http_internal_error(e)
 
+
 @router.get("/{custom_list_id}/parent")
 def get_custom_list_parent(
     *,
-    custom_list_id: CustomListName,
+    custom_list_id: str,
     current_user: AuthorizedUser,
 ) -> CustomListName | None:
     """
@@ -159,5 +160,8 @@ def get_custom_list_parent(
     :return: Name of parent custom list
     """
     if custom_list_id in CustomListParentMappings.parent_mappings:
-        return CustomListParentMappings.parent_mappings[custom_list_id]
+        try:
+            return CustomListParentMappings.parent_mappings[CustomListName(custom_list_id)]
+        except Exception as e:
+            raise_http_internal_error(e)
     return None
