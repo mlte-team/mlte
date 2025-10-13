717e277dc6150e6a29403739b7eaa01bcc416555
Jeffery Hansen <jhansen@sei.cmu.edu>
2025-10-13 10:20:31 -0400

    initial commit of gradient climber tests

---
+++ demo/GradientClimber/session.py
@@ -0,0 +1,13 @@
+import os
+from pathlib import Path
+
+from mlte.session import set_context, set_store
+
+
+# This script sets up the session context used for all steps of this demo, as well as
+# global constants about folders and model to use.
+
+store_path = Path.cwd() / ".." / "store"
+
+set_context("GradientClimber", "0.0.1")
+set_store(f"local://{store_path}")
