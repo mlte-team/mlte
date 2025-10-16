7c61afdd342703cfba16f1c4220465d194b266e9
Alex Derr <aderr@sei.cmu.edu>
2025-10-16 13:54:21 -0400

    redirect to index if accessing login page when logged in

--- mlte/frontend/nuxt-app/middleware/auth.global.ts
+++ mlte/frontend/nuxt-app/middleware/auth.global.ts
@@ -2,5 +2,7 @@ export default defineNuxtRouteMiddleware((from) => {
   const token = useCookie("token");
   if (!token.value && from.fullPath !== "/login-page") {
     return navigateTo("/login-page");
+  } else if (token.value && from.fullPath === "/login-page") {
+    return navigateTo("/");
   }
 });
