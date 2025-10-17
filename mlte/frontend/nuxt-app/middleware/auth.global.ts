034b15f6a977756ca48df0a829e5aaf21e62083f
Alex Derr <aderr@sei.cmu.edu>
2025-10-17 16:15:23 -0400

    Fix middleware bug

--- mlte/frontend/nuxt-app/middleware/auth.global.ts
+++ mlte/frontend/nuxt-app/middleware/auth.global.ts
@@ -1,8 +1,8 @@
 export default defineNuxtRouteMiddleware((from) => {
   const token = useCookie("token");
-  if (!token.value && from.fullPath !== "/login-page") {
+  if (!token.value && from.fullPath !== "/login-page/") {
     return navigateTo("/login-page");
-  } else if (token.value && from.fullPath === "/login-page") {
+  } else if (token.value && from.fullPath === "/login-page/") {
     return navigateTo("/");
   }
 });
