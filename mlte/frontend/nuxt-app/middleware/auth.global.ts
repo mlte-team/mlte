6fcec3109c86268209919b4f1988f8d71c27c000
Alex Derr <aderr@sei.cmu.edu>
2025-10-21 10:23:19 -0400

    Fix potential infinite nav bug

--- mlte/frontend/nuxt-app/middleware/auth.global.ts
+++ mlte/frontend/nuxt-app/middleware/auth.global.ts
@@ -1,7 +1,7 @@
 export default defineNuxtRouteMiddleware((from) => {
   const token = useCookie("token");
   if (!token.value && from.fullPath !== "/login-page/") {
-    return navigateTo("/login-page");
+    return navigateTo("/login-page/");
   } else if (token.value && from.fullPath === "/login-page/") {
     return navigateTo("/");
   }
