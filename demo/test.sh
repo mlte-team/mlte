96c0896ffdc5954351f245e80c02657641489add
Sebastian Echeverria <secheverria@sei.cmu.edu>
2025-10-23 16:29:48 -0400

    Demo: removed line that was accidentally duplicated

--- demo/test.sh
+++ demo/test.sh
@@ -12,9 +12,6 @@ cp -r ./sample_store ./store
 # Ensure proper order of notebooks.
 LC_COLLATE=C
 
-# Ensure proper order of notebooks.
-LC_COLLATE=C
-
 # Run all notebooks for the current demos.
 DEMOS="$@"
 for demo in ${DEMOS[@]}
