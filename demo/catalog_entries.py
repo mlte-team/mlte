abdbb0b9b28d7269b7d54563f1591e1d36ddd9fd
Alex Derr <aderr@sei.cmu.edu>
2025-10-23 16:05:57 -0400

    QA and updates to make qa and builds work

--- demo/catalog_entries.py
+++ demo/catalog_entries.py
@@ -3,6 +3,7 @@
 
 import json
 import os
+import re
 import subprocess
 import sys
 import tempfile
@@ -15,15 +16,16 @@ from mlte.catalog.model import CatalogEntry, CatalogEntryHeader
 
 
 def compare_entries(entry1: CatalogEntry, entry2: CatalogEntry) -> bool:
-    if (entry1.header.creator == entry2.header.creator and
-        entry1.header.updater == entry2.header.updater and
-        entry1.header.catalog_id == entry2.header.catalog_id and
-        entry1.tags == entry2.tags and
-        entry1.quality_attribute == entry2.quality_attribute and
-        entry1.code == entry2.code and
-        entry1.description == entry2.description and
-        entry1.inputs == entry2.inputs and
-        entry1.output == entry2.output
+    if (
+        entry1.header.creator == entry2.header.creator
+        and entry1.header.updater == entry2.header.updater
+        and entry1.header.catalog_id == entry2.header.catalog_id
+        and entry1.tags == entry2.tags
+        and entry1.quality_attribute == entry2.quality_attribute
+        and entry1.code == entry2.code
+        and entry1.description == entry2.description
+        and entry1.inputs == entry2.inputs
+        and entry1.output == entry2.output
     ):
         return True
     else:
@@ -37,21 +39,39 @@ if __name__ == "__main__":
 
     MODE = sys.argv[1]
     NOTEBOOK_PATH = Path(sys.argv[2])
-    SCRIPT_PATH = Path(f"./conversions/{Path(sys.argv[2]).parent}/{NOTEBOOK_PATH.stem}.py")
+    SCRIPT_PATH = Path(
+        f"/tmp/mlte/{Path(sys.argv[2]).parent}/{NOTEBOOK_PATH.stem}.py"
+    )
 
     timestamp = int(time.time())
     notebook_data = nbformat.read(NOTEBOOK_PATH, nbformat.NO_CONVERT)
     try:
-        notebook_entry_json = json.loads(notebook_data.cells[1].source)
-    except:
+        notebook_entry_data = notebook_data.cells[1].source
+        # Remove the trailing comma, so that it can be parsed
+        notebook_entry_data = re.sub(r",\s(})", "\n}", notebook_entry_data)
+        notebook_entry_json = json.loads(notebook_entry_data)
+    except Exception as e:
+        print(e)
         print(f"Misformatted entry data in {NOTEBOOK_PATH}.")
+        print(
+            f"Ensure that the second cell in the notebook contains JSON data for the sample test catalog."
+        )
         sys.exit(1)
     # Take the JSON entry data out of the notebook so that it doesn't end up in the code for the catalog entry
     notebook_data.cells.pop(1)
 
-    for key in ["identifier", "tags", "quality_attribute", "description", "inputs", "output"]:
+    for key in [
+        "identifier",
+        "tags",
+        "quality_attribute",
+        "description",
+        "inputs",
+        "output",
+    ]:
         if key not in notebook_entry_json:
-            print(f"Key, {key} was not provided in the entry data in, {NOTEBOOK_PATH}")
+            print(
+                f"Key, {key} was not provided in the entry data in {NOTEBOOK_PATH}"
+            )
             sys.exit(1)
 
     new_header = CatalogEntryHeader(
@@ -60,7 +80,7 @@ if __name__ == "__main__":
         created=timestamp,
         updater=None,
         updated=timestamp,
-        catalog_id="sample"
+        catalog_id="sample",
     )
     new_entry = CatalogEntry(
         header=new_header,
@@ -69,12 +89,28 @@ if __name__ == "__main__":
         code="",
         description=notebook_entry_json["description"],
         inputs=notebook_entry_json["inputs"],
-        output=notebook_entry_json["output"]
+        output=notebook_entry_json["output"],
     )
 
-    with tempfile.NamedTemporaryFile(mode="w", suffix=".ipynb") as temp_notebook_file:
+    with tempfile.NamedTemporaryFile(
+        mode="w", suffix=".ipynb"
+    ) as temp_notebook_file:
         nbformat.write(notebook_data, temp_notebook_file.name)
-        subprocess.run(["jupyter", "nbconvert", "--to", "script", "--output-dir", SCRIPT_PATH.parent, "--output", SCRIPT_PATH.stem, temp_notebook_file.name])
+        subprocess.run(
+            [
+                "jupyter",
+                "nbconvert",
+                "--to",
+                "script",
+                "--output-dir",
+                SCRIPT_PATH.parent,
+                "--output",
+                SCRIPT_PATH.stem,
+                temp_notebook_file.name,
+            ],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        )
 
     script = open(SCRIPT_PATH, "r").readlines()
     # Remove "#!/usr/bin/env python", # coding: utf-8\n" at the start and extra new line at the end
@@ -95,14 +131,16 @@ if __name__ == "__main__":
 
     timestamp = int(time.time())
     entry_file_name = SCRIPT_PATH.name.split("evidence_")[-1][:-3]
-    entry_path = Path(f"../mlte/store/catalog/sample/demo-{entry_file_name}.json")
+    entry_path = Path(
+        f"../mlte/store/catalog/sample/demo-{entry_file_name}.json"
+    )
     if os.path.exists(entry_path):
         with open(entry_path, "r") as entry_file:
             current_entry = CatalogEntry.from_json(json.load(entry_file))
 
     if MODE == "check":
         if not compare_entries(new_entry, current_entry):
-            print(f"Sample Catalog Entry: {entry_file_name}, was not updated.")
+            print(f"Sample Catalog Entry: {entry_file_name}, is not updated.")
             sys.exit(1)
         else:
             sys.exit(0)
