3236c5b9b99039dc7a2c9efeb4155787b40d8799
Alex Derr <aderr@sei.cmu.edu>
2025-10-27 11:41:24 -0400

    Docstrings, mode check, initial restructure

--- demo/catalog_entries.py
+++ demo/catalog_entries.py
@@ -1,5 +1,9 @@
-# Build mode: Take demo notebook script conversions and put them into the sample catalog
-# Check mode: Checks that all sample test catalog entries are updated
+"""
+Script that handles updating the sample catalog entries with the corresponding demo notebook.
+
+Build mode: Take demo notebook script conversions and put them into the sample catalog
+Check mode: Checks that all sample test catalog entries are updated
+"""
 
 import json
 import os
@@ -15,36 +19,27 @@ import nbformat
 from mlte.catalog.model import CatalogEntry, CatalogEntryHeader
 
 
-def compare_entries(entry1: CatalogEntry, entry2: CatalogEntry) -> bool:
-    if (
-        entry1.header.creator == entry2.header.creator
-        and entry1.header.updater == entry2.header.updater
-        and entry1.header.catalog_id == entry2.header.catalog_id
-        and entry1.tags == entry2.tags
-        and entry1.quality_attribute == entry2.quality_attribute
-        and entry1.code == entry2.code
-        and entry1.description == entry2.description
-        and entry1.inputs == entry2.inputs
-        and entry1.output == entry2.output
-    ):
-        return True
-    else:
-        return False
-
-
-if __name__ == "__main__":
+def main():
     if len(sys.argv) < 3:
         print("Required parameters: mode, notebook path.")
         sys.exit(1)
 
-    MODE = sys.argv[1]
-    NOTEBOOK_PATH = Path(sys.argv[2])
-    SCRIPT_PATH = Path(
-        f"/tmp/mlte/{Path(sys.argv[2]).parent}/{NOTEBOOK_PATH.stem}.py"
+    mode = sys.argv[1]
+    if mode not in ["build", "check"]:
+        print("Invalid mode. Valid modes are \"build\" and \"check\"")
+        sys.exit(1)
+    
+    notebook_path = Path(sys.argv[2])
+    timestamp = int(time.time())
+    script_path = Path(
+        f"/tmp/mlte/{Path(sys.argv[2]).parent}/{notebook_path.stem}.py"
+    )
+    entry_file_name = script_path.name.split("evidence_")[-1][:-3]
+    entry_path = Path(
+        f"../mlte/store/catalog/sample/demo-{entry_file_name}.json"
     )
 
-    timestamp = int(time.time())
-    notebook_data = nbformat.read(NOTEBOOK_PATH, nbformat.NO_CONVERT)
+    notebook_data = nbformat.read(notebook_path, nbformat.NO_CONVERT)
     try:
         notebook_entry_data = notebook_data.cells[1].source
         # Remove the trailing comma, so that it can be parsed
@@ -52,7 +47,7 @@ if __name__ == "__main__":
         notebook_entry_json = json.loads(notebook_entry_data)
     except Exception as e:
         print(e)
-        print(f"Misformatted entry data in {NOTEBOOK_PATH}.")
+        print(f"Misformatted entry data in {notebook_path}.")
         print(
             f"Ensure that the second cell in the notebook contains JSON data for the sample test catalog."
         )
@@ -70,7 +65,7 @@ if __name__ == "__main__":
     ]:
         if key not in notebook_entry_json:
             print(
-                f"Key, {key} was not provided in the entry data in {NOTEBOOK_PATH}"
+                f"Key, {key} was not provided in the entry data in {notebook_path}"
             )
             sys.exit(1)
 
@@ -103,16 +98,16 @@ if __name__ == "__main__":
                 "--to",
                 "script",
                 "--output-dir",
-                SCRIPT_PATH.parent,
+                script_path.parent,
                 "--output",
-                SCRIPT_PATH.stem,
+                script_path.stem,
                 temp_notebook_file.name,
             ],
             stdout=subprocess.DEVNULL,
             stderr=subprocess.DEVNULL,
         )
 
-    script = open(SCRIPT_PATH, "r").readlines()
+    script = open(script_path, "r").readlines()
     # Remove "#!/usr/bin/env python", # coding: utf-8\n" at the start and extra new line at the end
     script = script[3:-1]
     # Remove all the execution lines and extra new lines along with them
@@ -129,23 +124,41 @@ if __name__ == "__main__":
     script_str = script_str.replace(r'"', r"\"")
     new_entry.code = script_str
 
-    timestamp = int(time.time())
-    entry_file_name = SCRIPT_PATH.name.split("evidence_")[-1][:-3]
-    entry_path = Path(
-        f"../mlte/store/catalog/sample/demo-{entry_file_name}.json"
-    )
     if os.path.exists(entry_path):
         with open(entry_path, "r") as entry_file:
             current_entry = CatalogEntry.from_json(json.load(entry_file))
 
-    if MODE == "check":
+    if mode == "check":
         if not compare_entries(new_entry, current_entry):
             print(f"Sample Catalog Entry: {entry_file_name}, is not updated.")
             sys.exit(1)
         else:
             sys.exit(0)
 
-    elif MODE == "build":
+    elif mode == "build":
         if not compare_entries(new_entry, current_entry):
             with open(entry_path, "w") as entry_file:
                 json.dump(new_entry.to_json(), entry_file, indent=4)
+
+
+"""Compare two Catalog entries to see if they are identical, ignoring timestamps."""
+def compare_entries(entry1: CatalogEntry, entry2: CatalogEntry) -> bool:
+    if (
+        entry1.header.identifier == entry2.header.identifier
+        and entry1.header.creator == entry2.header.creator
+        and entry1.header.updater == entry2.header.updater
+        and entry1.header.catalog_id == entry2.header.catalog_id
+        and entry1.tags == entry2.tags
+        and entry1.quality_attribute == entry2.quality_attribute
+        and entry1.code == entry2.code
+        and entry1.description == entry2.description
+        and entry1.inputs == entry2.inputs
+        and entry1.output == entry2.output
+    ):
+        return True
+    else:
+        return False
+
+
+if __name__ == "__main__":
+    main()
