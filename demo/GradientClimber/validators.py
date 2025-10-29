25e3f529861cc65b13e49ef587f4befef58557f4
Jeffery Hansen <jhansen@sei.cmu.edu>
2025-10-29 09:22:55 -0400

    fixed error in threshold for functional correctness test.  added validator

--- demo/GradientClimber/validators.py
+++ demo/GradientClimber/validators.py
@@ -8,6 +8,24 @@ from mlte.evidence.types.array import Array
 from mlte.validation.validator import Validator
 
 
+def passed_percent_more_or_equal_then(threshold: float) -> Validator:
+    """
+    Checks if an array of pass/fail values have pass above a given threshold.
+
+    :param threshold: The threshold of required pass tests.
+    :return: A validator to check against this.
+    """
+    bool_exp: Callable[[Array], bool] = lambda value: sum(
+        g for g in value.array
+    ) >= threshold
+    validator: Validator = Validator.build_validator(
+        bool_exp=bool_exp,
+        success=f"The fraction of passes is at or over threshold {threshold}",
+        failure=f"The fraction of passes is below threshold  {threshold}",
+        input_types=[Array],
+    )
+    return validator
+
 def all_accuracies_more_or_equal_than(threshold: float) -> Validator:
     """
     Checks if the accuracy for multiple populations is fair by checking if all of them are over the given threshold.
