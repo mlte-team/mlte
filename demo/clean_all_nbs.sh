61975d31376b1f89cc9edf06a03686d9d89be725
Alex Derr <aderr@sei.cmu.edu>
2025-11-05 11:40:18 -0500

    Add iteration over subfolder notebooks

--- demo/clean_all_nbs.sh
+++ demo/clean_all_nbs.sh
@@ -9,4 +9,12 @@ do
   do
     source jq_clean_nb.sh "$notebook_file"
   done
+  # Iterate over any notebooks in subfolders of the demos
+  for subfolder_notebook_file in ${demo}/*/*.ipynb
+  do
+    echo $subfolder_notebook_file
+    if [ -f "$subfolder_notebook_file" ]; then
+      source jq_clean_nb.sh "$subfolder_notebook_file"
+    fi
+  done
 done
