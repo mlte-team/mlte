351fc0e9b463aca78d892f6d73dea07531df88f2
Sebastian Echeverria <secheverria@sei.cmu.edu>
2025-11-07 16:23:36 -0500

    Demo: fixing check for not running specific notebook that needs API key env var.

--- demo/test.sh
+++ demo/test.sh
@@ -19,7 +19,7 @@ do
   for file in ${demo}/*.ipynb
   do
     # Avoid running this notebook when the API key is missing, as it relies on an OpenAI API key that we cannot have configured by default
-    if [[ "$file" == "ReviewPro/2f_evidence_time_behavior.ipynb" ]] && [[ ! -v OPENAI_API_KEY ]]; then
+    if [[ "$file" == "ReviewPro/2f_evidence_time_behavior.ipynb" ]] && [[ -z "$OPENAI_API_KEY" ]]; then
       echo "Skipping notebook that requires unset API key env var."
     else
       poetry run pytest --nbmake "$file"
