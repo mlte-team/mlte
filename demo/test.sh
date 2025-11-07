4a073bf03fe88325043f13e62b4eaf354f06dd57
Sebastian Echeverria <secheverria@sei.cmu.edu>
2025-11-07 16:15:14 -0500

    Demo test: updating exception to only not run for that specific notebook if the API key is not set.

--- demo/test.sh
+++ demo/test.sh
@@ -18,8 +18,10 @@ for demo in ${DEMOS[@]}
 do 
   for file in ${demo}/*.ipynb
   do
-    # Avoid running this notebook because it relies on an OpenAI API key that we cannot have configured by default
-    if [[ "$file" != "ReviewPro/2f_evidence_time_behavior.ipynb" ]]; then
+    # Avoid running this notebook when the API key is missing, as it relies on an OpenAI API key that we cannot have configured by default
+    if [[ "$file" == "ReviewPro/2f_evidence_time_behavior.ipynb" ]] && [[ ! -v OPENAI_API_KEY ]]; then
+      echo "Skipping notebook that requires unset API key env var."
+    else
       poetry run pytest --nbmake "$file"
     fi
   done
