62b8abcea6659f723bcfc6f98a45199368bc751d
Alex Derr <aderr@sei.cmu.edu>
2025-11-24 10:29:39 -0500

    Add fs tests for cross validators

--- test/store/cross_validators/test_cross_validators.py
+++ test/store/cross_validators/test_cross_validators.py
@@ -1,76 +1,106 @@
 """Unit tests for CrossValidator functionality."""
 
+from pathlib import Path
 import pytest
 
 from mlte.artifact.type import ArtifactType
 from mlte.context.model import Model, Version
 from mlte.session.session_stores import SessionStores, setup_stores
 from mlte.store.artifact.store_session import ManagedArtifactSession
+from mlte.store.base import StoreType, StoreURI
 from mlte.store.catalog.store_session import ManagedCatalogSession
-from test.backend.fixture import test_api, user_generator
 from test.fixture.artifact import ArtifactModelFactory
 from test.store.artifact.test_underlying import check_artifact_writing
 from test.store.catalog.fixture import get_test_entry_for_store
-from test.backend.fixture.test_api import TestAPI
-from test.store.defaults import IN_MEMORY_SQLITE_DB
 
-URIS = [
-    "memory://",
-    IN_MEMORY_SQLITE_DB
-]
+# from test.store.defaults import IN_MEMORY_SQLITE_DB
+
+# IN_MEMORY_SQLITE_DB
+URIS = ["memory://", "fs://"]
 MODEL_ID = "model0"
 VERISON_ID = "version0"
 ARTIFACT_ID = "myid"
 VALID_USER = "admin"
 INVALID_USER = "not a user"
 
+# HTTP wasn't working, and rdbs isn't seeing custom_list entry table
+
 
 @pytest.mark.parametrize("store_uri", URIS)
-def test_artifact_cross_validators(store_uri: str):
+def test_artifact_cross_validators(store_uri: str, tmp_path: Path):
     """Test artifact cross validators."""
+
+    if StoreURI.from_string(store_uri).type == StoreType.LOCAL_FILESYSTEM:
+        store_uri += str(tmp_path)
 
     print(store_uri)
     stores = setup_stores(store_uri)
 
-    with ManagedArtifactSession(stores.artifact_store.session()) as artifact_store:
+    with ManagedArtifactSession(
+        stores.artifact_store.session()
+    ) as artifact_store:
         artifact_store.model_mapper.create(Model(identifier=MODEL_ID))
         artifact_store.version_mapper.create(
             Version(identifier=VERISON_ID), MODEL_ID
         )
-        
-        artifact = ArtifactModelFactory.make(ArtifactType.NEGOTIATION_CARD, ARTIFACT_ID)
+
+        artifact = ArtifactModelFactory.make(
+            ArtifactType.NEGOTIATION_CARD, ARTIFACT_ID
+        )
         artifact_id = artifact.header.identifier
 
         # Valid submission
         _ = check_artifact_writing(
-            artifact_store, MODEL_ID, VERISON_ID, artifact_id, artifact, VALID_USER
+            artifact_store,
+            MODEL_ID,
+            VERISON_ID,
+            artifact_id,
+            artifact,
+            VALID_USER,
         )
 
         # Invalid user submission
         with pytest.raises(RuntimeError):
             _ = check_artifact_writing(
-                artifact_store, MODEL_ID, VERISON_ID, artifact_id, artifact, INVALID_USER
-            ) 
+                artifact_store,
+                MODEL_ID,
+                VERISON_ID,
+                artifact_id,
+                artifact,
+                INVALID_USER,
+            )
 
         # Invalid quality attribute submission
         artifact.body.system_requirements[0].quality = "not a quality attribute"
         with pytest.raises(RuntimeError):
             _ = check_artifact_writing(
-                artifact_store, MODEL_ID, VERISON_ID, artifact_id, artifact, VALID_USER
+                artifact_store,
+                MODEL_ID,
+                VERISON_ID,
+                artifact_id,
+                artifact,
+                VALID_USER,
             )
 
         # TODO: make sure this is create and edit
 
 
 @pytest.mark.parametrize("store_uri", URIS)
-def test_catalog_cross_validators(store_uri: str):
+def test_catalog_cross_validators(store_uri: str, tmp_path: Path):
     """Test catalog cross validators."""
 
+    if StoreURI.from_string(store_uri).type == StoreType.LOCAL_FILESYSTEM:
+        store_uri += str(tmp_path)
+    
     stores = setup_stores(store_uri)
     entry = get_test_entry_for_store()
 
-    with ManagedCatalogSession(stores.catalog_stores.session()) as catalog_stores:
-        catalog_session = catalog_stores.get_session(SessionStores.LOCAL_CATALOG_STORE_ID)
+    with ManagedCatalogSession(
+        stores.catalog_stores.session()
+    ) as catalog_stores:
+        catalog_session = catalog_stores.get_session(
+            SessionStores.LOCAL_CATALOG_STORE_ID
+        )
 
         # Valid submission
         catalog_session.entry_mapper.create_with_header(entry, VALID_USER)
@@ -78,7 +108,7 @@ def test_catalog_cross_validators(store_uri: str):
         # Invalid user submission
         with pytest.raises(RuntimeError):
             catalog_session.entry_mapper.create_with_header(entry, INVALID_USER)
-        
+
         # Invalid quality attribute submission
         entry.quality_attribute = "not a quality attribute"
         with pytest.raises(RuntimeError):
