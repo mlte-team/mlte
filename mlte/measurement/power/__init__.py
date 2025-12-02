42735b9a6ba65a1b8aaff7383ab261bcc6982e46
Andrew O. Mellinger <aomellinger@sei.cmu.edu>
2025-12-02 15:02:40 -0500

    [skip ci]Added missing init.

---
+++ mlte/measurement/power/__init__.py
@@ -0,0 +1,10 @@
+from .nvidia_gpu_power_utilization import (
+    NvidiaGPUPowerStatistics,
+    NvidiaGPUPowerUtilization,
+)
+
+# TODO(Kyle): Find a more elegant way to do this
+__all__ = [
+    "NvidiaGPUPowerStatistics",
+    "NvidiaGPUPowerUtilization",
+]
