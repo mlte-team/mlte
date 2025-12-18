499fd61655a9d4ec009b2a0d147efc60c754a553
Alex Derr <aderr@sei.cmu.edu>
2025-12-18 11:10:35 -0500

    QA

--- test/tests/test_templating.py
+++ test/tests/test_templating.py
@@ -3,6 +3,7 @@
 from mlte.tests.templating import generate_suite_str
 from test.negotiation.test_artifact import get_sample_negotiation_card
 
+
 def test_generate_suite_str():
     """Test that the str generated is executable."""
     card = get_sample_negotiation_card()
