c7ab39c672f86ebc330e202a1a2f7ac9fe62ea50
Sebastian Echeverria <secheverria@sei.cmu.edu>
2026-03-03 14:20:49 -0500

    Session: ensured that set_credentials is easily accessible, similar to set_store and set_context

--- mlte/session/__init__.py
+++ mlte/session/__init__.py
@@ -1,3 +1,3 @@
-from .session import Session, session, set_context, set_store
+from .session import Session, session, set_context, set_credentials, set_store
 
-__all__ = ["session", "Session", "set_context", "set_store"]
+__all__ = ["session", "Session", "set_context", "set_store", "set_credentials"]
