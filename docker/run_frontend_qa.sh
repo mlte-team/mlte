c2c2d0cb2bae8a9c588d6177bb96a24cc9e35271
Alex Derr <aderr@sei.cmu.edu>
2026-03-20 10:31:15 -0400

    Update frontend QA to not mount

--- docker/run_frontend_qa.sh
+++ docker/run_frontend_qa.sh
@@ -6,6 +6,4 @@ set -e
 (cd .. && docker build -t mlte-frontend-qa . -f docker/Dockerfile.frontend_qa)
 
 docker run --rm \
-    -v "$(pwd)/../mlte:/mnt/app/mlte" \
-    --tmpfs /mnt/app/node_modules \
     mlte-frontend-qa "$@"
