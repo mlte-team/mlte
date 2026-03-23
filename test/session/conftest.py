bfac1f79af86cd7f54e5a81863c2e8795edef456
Sebastian Echeverria <secheverria@sei.cmu.edu>
2026-03-23 16:16:19 -0400

    Session Tests: readding fixtures that, while not used directly, have to be loaded so they can be indirectly called

--- test/session/conftest.py
+++ test/session/conftest.py
@@ -1,5 +1,13 @@
 """Loading common fixtures used in tests in this and child folders."""
 
-from test.store.conftest import patched_setup_stores
+from test.store.conftest import (
+    patched_create_engine,
+    patched_setup_stores,
+    shared_sqlite_engine,
+)
 
-__all__ = ["patched_setup_stores"]
+__all__ = [
+    "patched_setup_stores",
+    "patched_create_engine",
+    "shared_sqlite_engine",
+]
