5fc664185ab0052c3f00ab073fc9b10109c1e245
Alex Derr <aderr@sei.cmu.edu>
2026-04-28 11:12:49 -0400

    Full implemenation of custom list and user export tests

--- test/store/export/test_export.py
+++ test/store/export/test_export.py
@@ -9,49 +9,53 @@ from mlte.custom_list.custom_list_names import CustomListName
 from mlte.store.base import StoreType
 from mlte.store.catalog.store_session import ManagedCatalogSession
 from mlte.store.constants import LOCAL_CATALOG_STORE_ID
-from mlte.store.custom_list.store_session import ManagedCustomListSession
+from mlte.store.unified_store import UnifiedStore
+from mlte.store.user.policy import user_policy
 from mlte.store.user.store_session import ManagedUserSession
+from mlte.user.model import User
 from test.store.catalog.conftest import get_test_entry_for_store
 from test.store.conftest import create_test_unified_store
 from test.store.export.conftest import ALL_EXPORT_SPEC
-from test.store.user.test_underlying import get_test_user, setup_test_group
+from test.store.user.test_underlying import get_internal_store_session, get_test_user, setup_test_group
 from test.store.utils import store_types
 from mlte.store.export.export import _export_catalogs, _export_custom_lists, _export_users
-from test.store.user.conftest import create_test_user_store
-from test.store.custom_list.conftest import create_test_custom_list_store
 
 
 @pytest.mark.parametrize("store_type", store_types())
 def test_export_custom_lists(
     store_type: StoreType,
-    create_test_custom_list_store,
+    tmp_path: Path,
+    patched_setup_stores,
 ) -> None:
     """Test that custom lists can be exported."""
-    custom_list_store = create_test_custom_list_store(store_type)
+    stores: UnifiedStore = create_test_unified_store(store_type, tmp_path, patched_setup_stores)
 
-    all_export = _export_custom_lists(ALL_EXPORT_SPEC, custom_list_store)
+    all_export = _export_custom_lists(ALL_EXPORT_SPEC, stores.custom_list_store)
     for name in CustomListName:
         assert name in all_export
 
 
-# @pytest.mark.parametrize("store_type", store_types())
-# def test_export_users(
-#     store_type: StoreType,
-#     create_test_user_store
-# ) -> None:
-#     """Tests that users can be exported."""
-#     user_store = create_test_user_store(store_type)
+@pytest.mark.parametrize("store_type", store_types())
+def test_export_users(
+    store_type: StoreType,
+    tmp_path: Path,
+    patched_setup_stores,
+) -> None:
+    """Tests that users can be exported."""
+    stores: UnifiedStore = create_test_unified_store(store_type, tmp_path, patched_setup_stores)
+    test_user = get_test_user()
 
-    # test_user = get_test_user()
+    with ManagedUserSession(stores.user_store.session()) as user_store_session:
+        internal_store = get_internal_store_session(user_store_session, store_type)
+        test_user = user_policy.set_default_user_policies(
+            test_user, internal_store.policy_store
+        )
+        setup_test_group(user_store_session)
+        user_store_session.user_mapper.create(test_user)
 
-#     with ManagedUserSession(user_store.session()) as user_store_session:
-#         setup_test_group(user_store_session)
-#         user_store_session.user_mapper.create(test_user)
-
-#     export = _export_users(ALL_EXPORT_SPEC, user_store)
-#     print(export)
-#     assert test_user.username in export
-#     assert test_user in export[test_user.username]
+    export = _export_users(ALL_EXPORT_SPEC, stores.user_store)
+    assert test_user.username in export
+    assert test_user == User(**export[test_user.username])
 
 
 @pytest.mark.parametrize("store_type", store_types())
@@ -62,7 +66,6 @@ def test_export_catalogs(
 ) -> None:
     """Tests that local catalog can be exported."""
     stores = create_test_unified_store(store_type, tmp_path, patched_setup_stores)
-
     test_entry = get_test_entry_for_store(store_type=store_type)
 
     with ManagedCatalogSession(stores.catalog_stores.catalogs[LOCAL_CATALOG_STORE_ID].session()) as local_catalog_store:
